e0ea80343ca3f5b2dd648a2bc46e5364b7ecb0f7
KimBitbucketCloud <kim19961012@gmail.com>
2020-03-26 14:38:46 +0800

    no message

---
+++ interfaceSkills/Heal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaceSkills;
+
+import collisionproject.gameobject.Marble;
+
+/**
+ *
+ * @author kim19
+ */
+public class Heal implements Skills{
+
+    @Override
+    public void useSkill(Marble self, Marble target) {
+        System.out.print(self.getName() + " 使出治癒回復" + self.getAtk() + "點生命,");
+        self.setHp(self.getHp() + self.getAtk());
+        System.out.println(self.getName() + " 剩下血量" + self.getHp() + "點");
+    }
+    
+}
