cc7440bcb2935e911002d0b4e0a20c2bb2dae17e
KimBitbucketCloud <kim19961012@gmail.com>
2020-04-17 11:09:18 +0800

    loading完成

--- src/controllers/SceneController.java
+++ src/controllers/SceneController.java
@@ -12,6 +12,9 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import monsterstrike.LoadingThread;
 import monsterstrike.LoadingThread.*;
+import monsterstrike.gameobject.Dino;
+import monsterstrike.gameobject.ImgInfo;
+import monsterstrike.util.Global;
 import scenes.Scene;
 
 public class SceneController {
@@ -32,6 +35,7 @@ public class SceneController {
     private MouseCommandListener ml;
     private boolean isLoading;
     private BufferedImage img;
+    private Dino dino;
 
     public void changeScene(Scene scene) {
         if (this.currentScene != null) {
@@ -40,30 +44,41 @@ public class SceneController {
 
         isLoading = true;
         currentScene = scene;
+        this.dino = new Dino(ImgInfo.DINO, 650, 300, Dino.STEPS_RUN);
+        this.img = IRC.getInstance().tryGetImage(ImgInfo.LOADING_PATH);
         new LoadingThread(new Loading()).start();
     }
 
     public void sceneUpdate() {
         if (!isLoading) {
             this.currentScene.sceneUpdate();
+        }else{
+            this.dino.update();
         }
     }
 
     public KeyListener getKL() {
-        return this.kl;
+        if (!isLoading) {
+            return this.kl;
+        }
+        return null;
     }
 
     public MouseCommandListener getML() {
-        return this.ml;
+        if (!isLoading) {
+            return this.ml;
+        }
+        return null;
     }
 
     private int test = 0;
 
     public void paint(Graphics g) {
         if (this.isLoading) {
-            g.setColor(Color.white);
-            g.drawString((test++) + "", 600, 340);
-//            g.drawImage(img, 0, 0, Global.SCREEN_X, Global.SCREEN_Y, null);
+//            g.setColor(Color.white);
+//            g.drawString((test++) + "", 600, 340);
+            g.drawImage(img, 0, 0, Global.SCREEN_X, Global.SCREEN_Y, null);
+            this.dino.paint(g);
             return;
         }
         this.currentScene.paint(g);
